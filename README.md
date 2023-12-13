1470a86de0fb12edf3b8de33cf0abb8d7e32b4d0
Toki <codebadger1@gmail.com>
2023-12-13 22:15:26 +0200

    code overhaul

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 <h3>BeatsByToki</h3>
 
-[Live demo](https://okidokitokiloki.github.io/beatbox/)
+👉 [Live demo](https://okidokitokiloki.github.io/beatbox/)
 
 ## General info
-This is a simple application used to make a little noise. Nothing complicated, just having some fun with basic front end technology, <em>all vanilla</em>
+This is a simple application used to make a little noise. Nothing complicated, just having some fun with basic frontend technology
 <br>
 <br>
-<strong>tl;dr Sounds play when you push buttons</strong>
+<strong>Sounds play when you push buttons</strong>
 	
 ## Technologies
 Project is created with:
--- app.js
+++ app.js
@@ -1,105 +1,59 @@
 window.addEventListener('load', () => {
-
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pad");
     const tt = document.querySelectorAll("h3");
 
-    document.onkeydown = e =>{
+    document.addEventListener('keydown', e => {
+        const key = e.key;
+        const index = parseInt(key) - 1;
 
-        let key = e.keyCode;
+        if (!isNaN(index) && index >= 0 && index < sounds.length) {
+            playSound(index);
+            updateStyles(index, true);
+        }
+    });
 
-        if(key == 49 || key == 97){  // pressing #1
-            // playing a sound
-            sounds[0].volume = 0.3;
-            sounds[0].currentTime = 0;
-            sounds[0].play();
-            // styling the container
-            pads[0].style.transform = "scale(1.05)";
-            pads[0].style.borderRadius = "50%";
-            // styling the font
-            tt[0].style.fontSize = "2rem";
-            tt[0].style.color = "hsl(0, 0%, 86%)";
+    document.addEventListener('keyup', e => {
+        const key = e.key;
+        const index = parseInt(key) - 1;
+
+        if (!isNaN(index) && index >= 0 && index < sounds.length) {
+            updateStyles(index, false);
         }
-        else if(key == 50 || key == 98){  // pressing #2
-            sounds[1].volume = 0.3;
-            sounds[1].currentTime = 0;
-            sounds[1].play();
-            pads[1].style.transform = "scale(1.05)";
-            pads[1].style.borderRadius = "50%";
-            tt[1].style.fontSize = "2rem";
-            tt[1].style.color = "hsl(0, 0%, 86%)";
-        }
-        else if(key == 51 || key == 99){  // pressing #3
-            sounds[2].volume = 0.3;
-            sounds[2].currentTime = 0;
-            sounds[2].play();
-            pads[2].style.transform = "scale(1.05)";
-            pads[2].style.borderRadius = "50%";
-            tt[2].style.fontSize = "2rem";
-            tt[2].style.color = "hsl(0, 0%, 86%)";
-        }
-        else if(key == 52 || key == 100){  // pressing #4
-            sounds[3].volume = 0.3;
-            sounds[3].currentTime = 0;
-            sounds[3].play();
-            pads[3].style.transform = "scale(1.05)";
-            pads[3].style.borderRadius = "50%";
-            tt[3].style.fontSize = "2rem";
-            tt[3].style.color = "hsl(0, 0%, 86%)";
-        }
-        else if(key == 53 || key == 101){  // pressing #5
-            sounds[4].volume = 0.3;
-            sounds[4].currentTime = 0;
-            sounds[4].play();
-            pads[4].style.transform = "scale(1.05)";
-            pads[4].style.borderRadius = "50%";
-            tt[4].style.fontSize = "2rem";
-            tt[4].style.color = "hsl(0, 0%, 86%)";
-        }
-        else if(key == 54 || key == 102){  // pressing #6
-            sounds[5].volume = 0.3;
-            sounds[5].currentTime = 0;
-            sounds[5].play();
-            pads[5].style.transform = "scale(1.05)";
-            pads[5].style.borderRadius = "50%";
-            tt[5].style.fontSize = "2rem";
-            tt[5].style.color = "hsl(0, 0%, 86%)";
+    });
+
+    function playSound(index) {
+        const sound = sounds[index];
+        sound.volume = 0.3;
+        sound.currentTime = 0;
+        sound.play();
+    }
+
+    function updateStyles(index, keyPressed) {
+        const pad = pads[index];
+        const title = tt[index];
+
+        if (keyPressed) {
+            pad.style.transform = "scale(1.05)";
+            pad.style.borderRadius = "50%";
+            title.style.fontSize = "2rem";
+            title.style.color = "hsl(0, 0%, 86%)";
+        } else {
+            pad.style.transform = "scale(1)";
+            pad.style.borderRadius = "0";
+            title.style.fontSize = "1rem";
+            title.style.color = "hsl(240, 41%, 8%)";
         }
     }
 
-    document.onkeyup = e =>{ // Resetting all the styles back to 0
-
-        let key = e.keyCode;
-        if(key == 49 || key == 97){
-            pads[0].style.transform = "scale(1)";
-            pads[0].style.borderRadius = "0";
-            tt[0].style.fontSize = "1rem";
-            tt[0].style.color = "hsl(240, 41%, 8%)";
-        }else if(key == 50 || key == 98){
-            pads[1].style.transform = "scale(1)";
-            pads[1].style.borderRadius = "0";
-            tt[1].style.fontSize = "1rem";
-            tt[1].style.color = "hsl(240, 41%, 8%)";
-        }else if(key == 51 || key == 99){
-            pads[2].style.transform = "scale(1)";
-            pads[2].style.borderRadius = "0";
-            tt[2].style.fontSize = "1rem";
-            tt[2].style.color = "hsl(240, 41%, 8%)";
-        }else if(key == 52 || key == 100){
-            pads[3].style.transform = "scale(1)";
-            pads[3].style.borderRadius = "0";
-            tt[3].style.fontSize = "1rem";
-            tt[3].style.color = "hsl(240, 41%, 8%)";
-        }else if(key == 53 || key == 101){
-            pads[4].style.transform = "scale(1)";
-            pads[4].style.borderRadius = "0";
-            tt[4].style.fontSize = "1rem";
-            tt[4].style.color = "hsl(240, 41%, 8%)";
-        }else if(key == 54 || key == 102){
-            pads[5].style.transform = "scale(1)";
-            pads[5].style.borderRadius = "0";
-            tt[5].style.fontSize = "1rem";
-            tt[5].style.color = "hsl(240, 41%, 8%)";
-        }
-    }
+    (() => {
+        function showMessage() {
+            const overlayDiv = document.querySelector('.overlay');
+            overlayDiv.innerHTML = (window.innerWidth <= 560)
+                ? '<p>This webapp isn\'t meant for mobile devices.</p>'
+                : '<p>This webapp isn\'t meant for tablet devices.</p>';
+        };
+        showMessage();
+        window.addEventListener('resize', showMessage);
+    })();
 });
